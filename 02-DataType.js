ee6d149b450f2d898f82b39666619c3089002d4c
Jesus Canales <jadrian4b@gmail.com>
2025-09-17 01:22:39 -0700

    DataType

--- 02-DataType.js
+++ 02-DataType.js
@@ -10,10 +10,11 @@ let altura = 1.70
 //Booleanos
 let IsTeacher = true
 let IsStudent = false
+console.log(typeof IsStudent)
 
 //Undefined
 let undefined 
-console.log(undefined)
+console.log(typeof(undefined))
 
 /*Sirve para mostrar que no hay
 variable definida, pero en un
@@ -22,10 +23,24 @@ futuro poder poner una
 
 //Null
 let ValorNulo = null
-console.log(ValorNulo)
+console.log(typeof(ValorNulo))
 /*Esta variable es nula
 despues esperamos que esta 
 variable tenga un valor
 */
 
 //Symbol
+let Simbolo = Symbol("mySymbol")
+console.log(typeof(Simbolo))
+/*Identificador de propiedades
+intentar evitar colisiones y 
+es identificadores unicos*/
+
+//BigInt
+let MyBigInt1 = BigInt(12343243543321312321543)
+let MyBigInt2 = 12343243543321312321543n
+console.log(typeof(MyBigInt1))
+/*Nos sirve cuando intentamos 
+representar un numero muy grande
+que no puede ser representado de tipo
+Number */
