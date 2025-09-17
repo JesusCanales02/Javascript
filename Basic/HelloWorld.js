dd28433e92d5c39e99d54817423dc34e6197f810
Jesus Canales <jadrian4b@gmail.com>
2025-09-17 00:34:10 -0700

    Variables

--- Basic/HelloWorld.js
+++ Basic/HelloWorld.js
@@ -8,6 +8,8 @@ comentario
 console.log("'Hola mundo'")
 console.log('hola java')
 
-console.log(5)
-console.log("5")
-console.log(typeof("daw"))
+console.log(12)
+console.log("Soy jesus")
+console.log(typeof(5.3))
+console.log(typeof("5"))
+console.log(typeof("hola otra vez"))
---
+++ Basic/Variables.js
@@ -0,0 +1,25 @@
+//exiten 3 variables var, let y const
+
+//VAR
+//Puedes acceder a ella y cambiarla
+var nombre = "jesus"
+console.log(nombre)
+
+nombre = "ivan"
+console.log(nombre)
+
+//LET
+//Son son accesibles dentro de un bloque
+let nombre2 = "jesus adrian"
+console.log(nombre2)
+
+nombre2 = "jesus adrian 2"
+console.log(nombre2)
+
+//CONST
+//Es el valor que va a tener, no puede ser reasignada
+const nombre3 = "Xochitl"
+console.log(nombre3)
+
+nombre3 = "Ramon"
+console.log(nombre3)
