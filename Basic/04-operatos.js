cdc4d40b643d356218eb89f625bebb1800221db5
Jesus Canales <jadrian4b@gmail.com>
2025-09-24 20:23:25 -0700

    avance

---
+++ Basic/04-operatos.js
@@ -0,0 +1,51 @@
+//Operadores
+
+//Operadores aritmeticos
+let a = 4
+let b = 2
+console.log(a + b) //Suma
+console.log(a - b) //Resta
+console.log(a / b) //Division
+console.log(a * b) //Multiplicación
+console.log(a % b) //Modulo
+console.log(a ** b) //Exponente
+
+a++ //Incremento, se le suma 1
+console.log(a)
+
+b-- //Decremento, se le resta 1
+console.log(b)
+
+//Operadores de asignación
+let myVariable = 2
+console.log(myVariable)
+
+myVariable += 2
+console.log(myVariable)
+//----------------
+
+let myVariable1 = 2
+myVariable1 += 2                       //Esto es como poner  myVariable1 = myVariable1 + 2, solo se suma pero
+                                       //esto es mas corto
+console.log(myVariable1)
+
+let myVariable2 = 2
+myVariable2 -= 2
+console.log(myVariable2)
+
+let myVariable3 = 2
+myVariable3 *= 2
+console.log(myVariable3)
+
+let myVariable4 = 2
+myVariable4 /= 2
+console.log(myVariable4)
+
+let myVariable5 = 2
+myVariable5 %= 2
+console.log(myVariable5)
+
+let myVariable6 = 2
+myVariable6 **= 2
+console.log(myVariable6)
+
