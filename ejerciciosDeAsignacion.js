4c508301c2310687f731e3370a9b4c794bd41449
Jesus Canales <jadrian4b@gmail.com>
2026-04-11 22:38:51 -0700

    nbv

---
+++ ejerciciosDeAsignacion.js
@@ -0,0 +1,54 @@
+//operacion
+let numero1 = 2
+let numero2 = 4
+
+console.log(numero1 + numero2)
+console.log(numero1 - numero2)
+console.log(numero1 * numero2)
+console.log(numero1 / numero2)
+console.log(numero1 ** numero2)
+
+//incremento 
+let incrent = 6
+++incrent   //Aqui se suma uno
+console.log(incrent)
+
+var abcd = 3
+++abcd
+console.log(abcd) 
+
+var der = 9
+++der
+console.log(der) 
+
+//Ahora usaremos el (--) para restar 1
+let descre = 7 
+--descre
+console.log(descre)
+
+let other = 33
+--other
+console.log(other)
+
+let another = 0
+--another
+console.log(another)
+
+//operaciones de asignacion
+let nuevaAsignacion = 3
+nuevaAsignacion += 2     //es lo mismo de let nuevaAsignacion = nuevaasignacion + 2
+                        // nuevaasignacion = 3 + 2
+                        // nuevaasgniacaion = 5
+console.log(nuevaAsignacion)
+
+let asignacion = 4
+asignacion += 2
+console.log(asignacion)
+// asignacion = 4 + 2
+// resultado = 6
+
+let us = 5
+us += 3
+console.log(us)
+// us = 5 + 3
+// resultado = 8
