3d6cacd5bcf4251aec56859df8e6df0aa54b23bf
Jesus Canales <jadrian4b@gmail.com>
2026-04-19 00:47:58 -0700

    dwa

--- 06-strings.js
+++ 06-strings.js
@@ -33,4 +33,44 @@ console.log(greeting.at(-1))
 no sirve como en python que simplenete es greeting[-1]
 */
 
+//Metodos Comunes
 console.log(greeting.toUpperCase()) //Convierte todo a mayusculas
+console.log(greeting.toLowerCase()) //Convierte todo a minuscula
+console.log(greeting.indexOf("Jesus")) //Busca la palabra en el texto y dice en que indice esta
+console.log(greeting.indexOf("Hola"))
+console.log(greeting.includes("Jesus")) //Busca la palabra si esta en la variable si esta TRUE si no FALSE
+console.log(greeting.includes("!")) 
+console.log(greeting.includes("Siii"))
+console.log(greeting.slice(0, 5)) // Solo sirve para retornar un pedazo de un indice a otro
+console.log(greeting.replace("Jesus", "Sergio")) // Aqui simplemente hacemos un cambio de palabras existentes,
+//La palabra que se cambio fue "Jesus" por "Sergio", si no existe no hace cambio
+console.log(greeting.replace("jesus", "sergio")) //No funciona porque no existe "jesus" solo "Jesus"
+
+
+//Template literals (plantillas literales)
+//Sirven para crear strings que sean de varias lineas o interporar variables por un lado
+
+let message = `Es mi
+curso de 
+javascript`
+
+console.log(message)
+
+let mensaje = `Yo siempre
+y mi mala
+suerte`
+console.log(mensaje)
+
+console.log(`Hii!! yo soy ${Myname}`)
+console.log(`Asi ee, ${greeting}`)
+
+/*
+EJEMPLO...
+let word = "apoco si"
+let other = "Mi richard " + word
+console.log(word)
+console.log(other)
+console.log(other[0])
+console.log(word.at(-1))
+console.log(other.toUpperCase())
+*/
