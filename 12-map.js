e78cb74a5e52ebf81cc94f669a304edfc678051e
Jesus Canales <jadrian4b@gmail.com>
2026-05-11 22:38:30 -0700

    Map

---
+++ 12-map.js
@@ -0,0 +1,13 @@
+//Map
+
+//Declaracion
+let myMap = new Map()
+console.log(myMap)
+
+//Inicializacion
+myMap = new Map([
+    ["name", "Jesus"],
+    ["email", "jesus.canales@cetys.edu.mx"],
+    ["edad", 23]
+])
+console.log(myMap)
