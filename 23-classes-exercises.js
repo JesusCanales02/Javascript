3e96f041851fa71779eea61c6b283652ec55aeac
Jesus Canales <jadrian4b@gmail.com>
2026-06-21 00:25:03 -0700

    Classes finished

--- 23-classes-exercises.js
+++ 23-classes-exercises.js
@@ -4,21 +4,143 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=18630
 */
 
 // 1. Crea una clase que reciba dos propiedades
-
+class Nombre {
+    constructor(name, lastname) {
+        this.name = name,
+        this.lastname = lastname
+    }
+}
+let Yo = new Nombre("jesus", "canales")
+console.log(Yo)
 // 2. Añade un método a la clase que utilice las propiedades
-
+class MiNombre {
+    constructor(name, lastname) {
+        this.name = name,
+        this.lastname = lastname
+    }
+    saludar(){
+        console.log(`Hola ${this.name}!!, tu apellido es ${this.lastname} verdad?`)
+    }
+}
+let otro = new MiNombre("Jesús", "Canales")
+console.log(otro)
+otro.saludar()
 // 3. Muestra los valores de las propiedades e invoca a la función
-
+console.log(otro.name)
+console.log(otro.lastname)
+otro.saludar()
 // 4. Añade un método estático a la primera clase
-
+class MiApellido {
+    constructor(name, lastname) {
+        this.name = name,
+        this.lastname = lastname
+    }
+    static saludar(alguien){
+        return `Hola ${alguien}!!`
+    }
+}
 // 5. Haz uso del método estático
-
+console.log(MiApellido.saludar("jesus"))
 // 6. Crea una clase que haga uso de herencia
+class Animal {
+    constructor(nombre, raza = "sin raza") {
+        this.nombre = nombre,
+        this.raza = raza
+    }
+    ruido(){
+        console.log("Ruido generico")
+    }
+}
 
+class gato extends Animal {
+    constructor(nombre, altura) {
+        super(nombre)
+        this.altura = altura
+    }
+    ruido(){
+        console.log("Miauu")
+    }
+}
+let Migato = new gato("Sam", 34)
+console.log(Migato)
+Migato.ruido()
 // 7. Crea una clase que haga uso de getters y setters
-
+/*
+class Cuenta {
+    constructor(costo, comida) {
+        this.valorTotal = costo,
+        this.comida = comida
+    }
+    get costo(){
+        let mostrar = `Su costo fue de ${this.valorTotal}`
+        return mostrar
+    }
+    set costo(descuento){
+        if (descuento > 2000) {
+            descuento /= 2
+            this.valorTotal = descuento
+        }else{
+            this.valorTotal = descuento
+        }
+    }
+}
+let persona = new Cuenta(3231,"Tacos")
+console.log(persona.costo)
+persona.costo = 6000
+console.log(persona.costo)
+persona.costo = 1826
+console.log(persona.costo)
+*/
 // 8. Modifica la clase con getters y setters para que use propiedades privadas
-
+class Cuenta {
+    #costo
+    constructor(costo, comida) {
+        this.#costo = costo,
+        this.comida = comida
+    }
+    get costo(){
+        let mostrar = `Su costo fue de ${this.#costo}`
+        return mostrar
+    }
+    set costo(descuento){
+        if (descuento > 2000) {
+            descuento /= 2
+            this.#costo = descuento
+        }else{
+            this.#costo = descuento
+        }
+    }
+}
+let persona = new Cuenta(3231,"Tacos")
+console.log(persona.costo)
+persona.costo = 6000
+console.log(persona.costo)
+persona.costo = 1824
+console.log(persona.costo)
 // 9. Utiliza los get y set y muestra sus valores
+console.log(persona.costo)
+persona.costo = 3402
+console.log(persona.costo)
 
 // 10. Sobrescribe un método de una clase que utilice herencia 
+class Pizza {
+    constructor(nombre, precio) {
+        this.nombre = nombre,
+        this.precio = precio
+    }
+    grito(){
+        console.log("La pizza ya esta lista!!")
+    }
+}
+class Orden extends Pizza{
+    constructor(nombre, medida) {
+        super(nombre)
+        this.medida = medida
+    }
+    grito(){
+        console.log("La pizza de peperoni ya salio vengan por ella!!")
+    }
+}
+let cliente = new Orden("Peperoni", "Mediana")
+
+cliente.grito()
--- ejercicios-classes-IA.js
+++ ejercicios-classes-IA.js
@@ -81,3 +81,174 @@ console.log(price.precio)
 price.precio = 46
 console.log(price.precio)
 
+//clases
+class Persona {
+    constructor(name, age, nickname) {
+        this.name = name,
+        this.age = age,
+        this.nickname = nickname
+    }
+}
+//sintaxis
+let persona1 = new Persona("Ramon", 32, "Cabron")
+console.log(persona1)
+console.log(persona1.name)
+
+//metodo
+class Perro {
+    constructor(raza, nombre, edad) {
+        this.raza = raza,
+        this.nombre = nombre,
+        this.edad = edad
+    }
+    ladra(apodo){
+        console.log(`El ${apodo} esta ladrando`)
+    }
+}
+//Instancia
+let perro1 = new Perro("pastor aleman", "lobo", 3)
+perro1.ladra("Lobo")
+
+//Clases por default
+class ClassDefault {
+    constructor(name = "default", age = 0, color = "Sin color") {
+        this.name = name,
+        this.age = age,
+        this.color = color        
+    }
+}
+let Midefault = new ClassDefault() //Podemos poner el nombre, edad y color y saldrán
+console.log(Midefault)
+
+//Propiedad privada
+class MiPrivado {
+    #bank
+    constructor(name, edad, bank) {
+        this.name = name,
+        this.edad = edad,
+        this.#bank = bank
+    }
+    //Sin metodo
+}
+let perosna3 = new MiPrivado("jesus", 32, "23432ds")
+console.log(perosna3)
+console.log(perosna3.bank)
+
+//Ejercicios de getters y settes
+
+class Auto {
+    #velocidad
+    constructor(brand, velocidad) {
+        this.brand = brand,
+        this.#velocidad = velocidad
+    }
+    get velocidad(){
+        return `La velocidad: ${this.#velocidad} km/h`
+    }
+    set velocidad(nuevaVelocidad){
+        if (nuevaVelocidad >= 0 && nuevaVelocidad <= 240){
+            this.#velocidad = nuevaVelocidad
+            return nuevaVelocidad
+        }else{
+            console.log("Velocidad peligrosa o invalida")
+        }
+    }
+}
+let Micarro = new Auto("Ferrari", 0)
+console.log(Micarro)
+Micarro.velocidad
+Micarro.velocidad = 100
+console.log(Micarro.velocidad)
+
+//Segundo ejercicio
+class Termostato {
+    #temperatura
+    constructor(temperatura = 22) {
+        this.#temperatura = temperatura
+    }
+    get temperatura(){
+        return `La temperatura es ${this.#temperatura} grados`
+    }
+    set temperatura(NuevaTemp){
+        if (NuevaTemp >= 16 && NuevaTemp <= 30) {
+            this.#temperatura = NuevaTemp
+            return NuevaTemp
+        }else{
+            console.log("Temperatura fuera de los limites permitidos")
+        }
+    }
+}
+let Mitemperatura = new Termostato()
+console.log(Mitemperatura)
+Mitemperatura.temperatura = 23
+console.log(Mitemperatura.temperatura)
+
+class Usuario {
+    #password
+    constructor(username, password) {
+        this.username = username,
+        this.#password = password
+    }
+    get password(){
+        return "Password protegido"
+    }
+    set password(nuevaPassword){
+        if (nuevaPassword.length >= 8) {
+            this.#password = nuevaPassword
+            console.log(`The password: ${nuevaPassword}`)            
+        }else{
+            console.log("Error, password must have min 8 caracters")
+        }
+    }
+}
+let Miusuario = new Usuario("Jesus123")
+console.log(Miusuario)
+Miusuario.password = "12231cdscdsf"
+Miusuario.password
+
+//Herencia
+//ejercicios
+class Personaje {
+    constructor(nombre) {
+        this.nombre = nombre
+    }
+}
+
+class Superheroe extends Personaje {
+    constructor(nombre, superpoder) {
+        super(nombre)
+        this.superpoder = superpoder
+    }
+    usarPoder(){
+        console.log(`${this.nombre} usa el poder de ${this.superpoder}`)
+    }
+}
+let MiSuper = new Superheroe("Spiderman", "escalar")
+console.log(MiSuper)
+MiSuper.usarPoder()
+
+class Empleado {
+    trabajar() {
+        console.log("El empleado esta trabajando")
+    }
+}
+class Repartidor extends Empleado{
+    trabajar(){
+        console.log("El repartidor esta entregando una pizza")
+    }
+}
+let empleado1 = new Empleado()
+empleado1.trabajar()
+let persona45 = new Repartidor()
+persona45.trabajar()
+
+class Convertidor {
+    static CelsiusAFahrenheit(celsius) {
+        return (celsius * 9/5) + 32
+    }
+    static kmMillas(km){
+        return km * 0.621371
+    }
+}
+console.log(Convertidor.CelsiusAFahrenheit(3))
+console.log(Convertidor.kmMillas(4))
